9c9c07d93020fe34a27d963a549a8d086565de57
nadersam <nadersam@hotmail.com>
2023-12-26 12:42:54 +0200

    dental web project
    dental web project

---
+++ Scripts/MainScript.js
@@ -0,0 +1,134 @@
+﻿$(document).ready(function () {
+    vm = new MainViewModel();
+    //$("#BirthDateControl").datepicker({ dateFormat: 'dd-mm-yy', changeYear: true });
+    //$("#BirthDateControl").mask("99-99-9999");
+    ko.applyBindings(vm);
+});
+var MainViewModel = function () {
+    var self = this;
+    self.UserName = ko.observable("");
+    self.PassWord = ko.observable("");
+    self.ValidateLogin = function () {
+        if (self.UserName() == "") {
+            toastr.warning("Please enter a valid user name.");
+            return;
+        }
+        if (self.PassWord() == "") {
+            toastr.warning("Please enter a valid user name.");
+            return;
+        }
+        $.ajax({
+            url: "/DentalAPI/api/Dental/ValidateLogin?UserName=" + self.UserName() + "&Password=" + self.PassWord(),
+            type: 'GET',
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data != 0) {
+                    toastr.success("Successfull Login.");
+                    window.location = "/DentalWeb/DashBoard.html?UserName=" + self.UserName() + "&UserKey=" + data;
+                } else {
+                    toastr.warning("Invalid user name or password.");
+                }
+            },
+            error: function (request) {
+                alert(request.responseText);
+            }
+        });
+    };
+    self.Genders = ko.observableArray([]);
+    self.FillGenders = function () {
+        self.Genders([]);
+        self.Genders.push(new CodeDescEntity({ Code: "1", Description: "Male" }));
+        self.Genders.push(new CodeDescEntity({ Code: "2", Description: "Female" }));
+    }
+    self.Patients = ko.observableArray([]);
+    self.PatientID = ko.observable("");
+    self.Name1 = ko.observable("");
+    self.Name2 = ko.observable("");
+    self.Name3 = ko.observable("");
+    self.Phone = ko.observable("");
+    self.GetPats = function () {
+        var pid;
+        if (self.PatientID() == "" || self.PatientID() == undefined || self.PatientID() == 0) {
+            pid = "0";
+        } else {
+            pid = self.PatientID();
+        }
+        $.ajax({
+            url: "/DentalAPI/api/Dental/GetPatsList?PatientID=" + pid + "&Name1=" + self.Name1() + "&Name2=" + self.Name2() + "&Name3=" + self.Name3() + "&Phone=" + self.Phone(),
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                var pats = ko.utils.arrayMap(data, function (item) {
+                    return new PatientEntity(item)
+                });
+                self.Patients(pats);
+            },
+            error: function (request) {
+                alert(request.responseText);
+            },
+        });
+    }
+    self.SPatientID = ko.observable("");
+    self.SLName1 = ko.observable("");
+    self.SLName2 = ko.observable("");
+    self.SLName3 = ko.observable("");
+    self.SAName1 = ko.observable("");
+    self.SAName2 = ko.observable("");
+    self.SAName3 = ko.observable("");
+    self.SPhone = ko.observable("");
+    self.SBirthDate = ko.observable("");
+    self.SEmail = ko.observable("");
+    self.SAddress = ko.observable("");
+    self.SGender = ko.observable("");
+    self.LoadPatData = function (PatientID) {
+        self.FillGenders();
+        $.ajax({
+            url: "/DentalAPI/api/Dental/LoadPatData?PatientID=" + PatientID,
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                self.SPatientID(data[0].PatientKey);
+                self.SLName1(data[0].LName1);
+                self.SLName2(data[0].LName2);
+                self.SLName3(data[0].LName3);
+                self.SAName1(data[0].AName1);
+                self.SAName2(data[0].AName2);
+                self.SAName3(data[0].AName3);
+                self.SPhone(data[0].Phone);
+                self.SBirthDate(data[0].BirthDate);
+                self.SEmail(data[0].Email);
+                self.SAddress(data[0].Address);
+                self.SGender(data[0].Gender);
+            },
+            error: function (request) {
+                alert(request.responseText);
+            },
+        });
+    }
+    self.LoadPatEdit = function (item) {
+        self.LoadPatData(item.PatientKey());
+        $("#PatEditModal").modal('show');
+    }
+    self.PatEditModalSubmit = function () {
+        $("#PatEditModal").modal('hide');
+    }
+}
+function PatientEntity(item) {
+    var self = this;
+    self.PatientKey = ko.observable(item.PatientKey);
+    self.Phone = ko.observable(item.Phone);
+    self.BirthDate = ko.observable(item.BirthDate);
+    self.Gender = ko.observable(item.Gender);
+    self.Address = ko.observable(item.Address);
+    self.Email = ko.observable(item.Email);
+    self.LName = ko.observable(item.LName);
+    self.AName = ko.observable(item.AName);
+}
+function CodeDescEntity(item) {
+    var self = this;
+    self.Code = ko.observable(item.Code);
+    self.Description = ko.observable(item.Description);
+}
---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+/* chart.js chart examples */
+
+// chart colors
+var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+
+/* large line chart */
+var chLine = document.getElementById("chLine");
+var chartData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    data: [589, 445, 483, 503, 689, 692, 634],
+    backgroundColor: 'transparent',
+    borderColor: colors[0],
+    borderWidth: 4,
+    pointBackgroundColor: colors[0]
+  }
+//   {
+//     data: [639, 465, 493, 478, 589, 632, 674],
+//     backgroundColor: colors[3],
+//     borderColor: colors[1],
+//     borderWidth: 4,
+//     pointBackgroundColor: colors[1]
+//   }
+  ]
+};
+if (chLine) {
+  new Chart(chLine, {
+  type: 'line',
+  data: chartData,
+  options: {
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    responsive: true
+  }
+  });
+}
+
+/* large pie/donut chart */
+var chPie = document.getElementById("chPie");
+if (chPie) {
+  new Chart(chPie, {
+    type: 'pie',
+    data: {
+      labels: ['Desktop', 'Phone', 'Tablet', 'Unknown'],
+      datasets: [
+        {
+          backgroundColor: [colors[1],colors[0],colors[2],colors[5]],
+          borderWidth: 0,
+          data: [50, 40, 15, 5]
+        }
+      ]
+    },
+    plugins: [{
+      beforeDraw: function(chart) {
+        var width = chart.chart.width,
+            height = chart.chart.height,
+            ctx = chart.chart.ctx;
+        ctx.restore();
+        var fontSize = (height / 70).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        var text = chart.config.data.datasets[0].data[0] + "%",
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      }
+    }],
+    options: {layout:{padding:0}, legend:{display:false}, cutoutPercentage: 80}
+  });
+}
+
+/* bar chart */
+var chBar = document.getElementById("chBar");
+if (chBar) {
+  new Chart(chBar, {
+  type: 'bar',
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+      data: [589, 445, 483, 503, 689, 692, 634],
+      backgroundColor: colors[0]
+    },
+    {
+      data: [639, 465, 493, 478, 589, 632, 674],
+      backgroundColor: colors[1]
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        barPercentage: 0.4,
+        categoryPercentage: 0.5
+      }]
+    }
+  }
+  });
+}
+
